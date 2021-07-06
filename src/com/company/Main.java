0be1264b6e54cf8237d7024f4d7e56be7315a4fb
joaope-code <jaumundergroud@gmail.com>
2021-07-06 15:21:48 -0300

    block s

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int n1 =3, n2 = 4;
+        System.out.println(n1+n2);
+
     }
 }
